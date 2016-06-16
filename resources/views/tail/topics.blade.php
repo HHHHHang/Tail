778650141b5dd48b9ecacbb15f57b82097f79f83
lyn <tongji_lyn@163.com>
2016-06-17 00:51:20 +0800

    bug fix
    rt

--- resources/views/tail/topics.blade.php
+++ resources/views/tail/topics.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	@include('tail.layout.header', ['active' => ''])
+	@include('tail.layout.lib')
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
