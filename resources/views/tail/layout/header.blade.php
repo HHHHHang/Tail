afaaefbfc9d14c67b18fc3fc6e230287deefb892
林悦锵 <410787046@qq.com>
2016-06-17 14:40:45 +0800

    更新：navigation bar，footer

--- resources/views/tail/layout/header.blade.php
+++ resources/views/tail/layout/header.blade.php
@@ -11,18 +11,15 @@
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                    <li class="{{$active=='index' ? 'active ' : ''}}"><a href="/">首页</a></li>
-                    <li class="{{$active=='bbs' ? 'active ' : ''}} dropdown">
-                        <a href="/forum" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">论坛 <span class="caret"></span></a>
+                    <li class="{{$active=='index' ? 'active' : ''}}"><a href="/">首页</a></li>
+                    <li class="{{$active=='article' ? 'active' : ''}}"><a href="/forum">文章</a></li>
+                    <li class="{{$active=='bbs' ? 'active' : ''}} dropdown">
+                        <a href="/forum/tie" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">论坛 <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="/forum">精选文章</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li class="dropdown-header">帖子专区</li>
                             <li><a href="/forum/tie">纠结帖子</a></li>
                             <li><a href="#">其它帖子</a></li>
                         </ul>
                     </li>
-                    <li><a href="/search/article">搜索</a></li>
 
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
@@ -34,6 +31,25 @@
                         <li><a href="/login">注册</a></li>
                     @endif
                 </ul>
+
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-btn">
+                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">标题<span class="caret"></span></button>
+                                <ul class="dropdown-menu dropdown-menu-left">
+                                    <li><a href="#">文章</a></li>
+                                    <li><a href="#">帖子</a></li>
+                                    <li><a href="#">话题</a></li>
+                                </ul>
+                            </div>
+                            <input type="text" class="form-control" placeholder="搜索">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">搜索</button>
+                            </span>
+                        </div>
+                    </div>
+                </form>
             </div><!--/.nav-collapse -->
         </div>
     </nav>
